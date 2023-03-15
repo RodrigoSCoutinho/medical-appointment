bbadb7042e375c7e1337dd52e01de9d0da4d88fe
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-03-15 08:03:15 -0300

    correct bugs and errors

--- src/modules/doctor/useCases/create-doctor/tests/create-doctor.usecase.test.ts
+++ src/modules/doctor/useCases/create-doctor/tests/create-doctor.usecase.test.ts
@@ -1,24 +1,39 @@
-import { SpecialityMemoryRepository } from './../../../../speciality/repositories/implementations/speciality.memory.repository';
-import { randomUUID } from 'crypto';
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, beforeAll } from "vitest";
 import { UserMemoryRepository } from '../../../../users/repositories/implementations/user.memory.repository';
 import { DoctorMemoryRepository } from '../../../repositories/implementations/doctor-memory.repository';
 import { CreateDoctorRequest, CreateDoctorUseCase } from '../create-doctor.usecase';
+import { SpecialityMemoryRepository } from './../../../../speciality/repositories/implementations/speciality.memory.repository';
+import { Speciality } from '../../../../speciality/entities/speciality.entity';
+import { ISpecialityRepository } from '../../../../speciality/repositories/speciality.repository';
+
+let specialityRepository: ISpecialityRepository;
+let speciality: Speciality;
+
+beforeAll(async () => {
+  specialityRepository = new SpecialityMemoryRepository();
+ 
+  speciality = Speciality.create({
+    description: 'DESC_TEST',
+    name:'NAME_TEST'
+  });
+
+  await specialityRepository.save(speciality)
+})
 
 describe("Create Doctor UseCase", () => {
     test('Should be able to create a new Doctor', async () => {
+     
+      const userRepository = new UserMemoryRepository();
+      const doctorRepository = new DoctorMemoryRepository();
+    
       const doctorMock: CreateDoctorRequest = {
         username: 'username_test',
         name: 'name_test',
         password: 'password_test',
         email: 'email@gmail.com',
         crm: '123456',
-        specialityId: randomUUID(),
+        specialityId: speciality.id
       }
-
-      const userRepository = new UserMemoryRepository();
-      const doctorRepository = new DoctorMemoryRepository();
-      const specialityRepository = new SpecialityMemoryRepository();
 
       const createDoctorUseCase = new CreateDoctorUseCase(
         userRepository, 
@@ -32,13 +47,16 @@ describe("Create Doctor UseCase", () => {
     })
 
     test('Should not be able to create a new Doctor with exists CRM', async () => {
+      const userRepository = new UserMemoryRepository();
+      const doctorRepository = new DoctorMemoryRepository();
+    
       const doctorMock: CreateDoctorRequest = {
         username: 'username_test',
         name: 'name_test',
         password: 'password_test',
         email: 'email@gmail.com',
         crm: '123456',
-        specialityId: randomUUID(),
+        specialityId: speciality.id
       }
       const doctorMockDuplicated: CreateDoctorRequest = {
         username: 'username_duplicated',
@@ -46,12 +64,9 @@ describe("Create Doctor UseCase", () => {
         password: 'password_duplicated',
         email: 'email@duplicatedgmail.com',
         crm: '123456',
-        specialityId: randomUUID(),
+        specialityId: speciality.id
       }
 
-      const userRepository = new UserMemoryRepository();
-      const doctorRepository = new DoctorMemoryRepository();
-      const specialityRepository = new SpecialityMemoryRepository();
 
       const createDoctorUseCase = new CreateDoctorUseCase(
         userRepository, 
@@ -74,19 +89,19 @@ describe("Create Doctor UseCase", () => {
         password: 'password_test',
         email: 'email@gmail.com',
         crm: '12345',
-        specialityId: randomUUID(),
+        specialityId: speciality.id,
       }
 
       const userRepository = new UserMemoryRepository();
       const doctorRepository = new DoctorMemoryRepository();
-      const specialityRepository = new SpecialityMemoryRepository();
 
       const createDoctorUseCase = new CreateDoctorUseCase(
         userRepository, 
         doctorRepository,
-        specialityRepository);
+        specialityRepository
+        );
 
-        expect(async () => {
+      expect(async () => {
         await createDoctorUseCase.execute(doctorMock);
       }).rejects.toThrow('CRM length is incorrect!');
    })
