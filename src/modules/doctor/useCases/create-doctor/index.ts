8099d8dc7f5ac8fe8c209b8d7ccc09571159c2e1
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-03-22 07:56:42 -0300

    implementations ready

---
+++ src/modules/doctor/useCases/create-doctor/index.ts
@@ -0,0 +1,14 @@
+import { SpecialityPrismaRepository } from "../../../speciality/repositories/implementations/speciality.prisma.repository";
+import { UserPrismaRepository } from "../../../users/repositories/implementations/user.prisma.repository";
+import { DoctorPrismaRepository } from "../../repositories/implementations/doctor-prisma.repository";
+import { CreateDoctorController } from "./create-doctor.controller";
+
+
+const userRepository = new UserPrismaRepository()
+const doctorRepository = new DoctorPrismaRepository();
+const specilityRepository = new SpecialityPrismaRepository()
+
+const createDoctorController = new CreateDoctorController(userRepository, doctorRepository, specilityRepository);
+
+
+export {createDoctorController}
