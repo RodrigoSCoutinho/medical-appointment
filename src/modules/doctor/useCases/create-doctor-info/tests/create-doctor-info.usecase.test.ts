32eed3ea36e17d184e9738c05fbbbd9ecf88a4ef
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-04-04 08:45:10 -0300

    adding the dayjs and create test

--- src/modules/doctor/useCases/create-doctor-info/tests/create-doctor-info.usecase.test.ts
+++ src/modules/doctor/useCases/create-doctor-info/tests/create-doctor-info.usecase.test.ts
@@ -1,5 +1,17 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
+import dayjs from "dayjs"
 
 describe('Create doctor info',() => {
-    test()
+    test('Should not able to create a doctor info if doctor does not exists!', () => {
+      const createDoctorInfoUseCase = new CreateDoctorInfoUseCase();
+      
+      expect(async () => {
+        await createDoctorInfoUseCase.execute({
+            startAt: dayjs().startOf('day').add(10, 'hour').format('HH:mm'),
+            endAt: '',
+            price: 150,
+            duration: 10
+          })
+       }).rejects.toThrow('Doctor does not exists!')
+    });
 })
