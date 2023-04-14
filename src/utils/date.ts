5a1bbb94be69b2b9fbd04d469357b116834b9e91
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-04-14 20:50:44 -0300

    changed the function for validate the time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,14 @@
-// validate the time 
+import dayjs from "dayjs";
 
-//validate if the time is bigger than the start 
+//! Validate the time and hour from dayjs
+
+
+function validateTime(time: string){
+
+}
+
+function formatDateHour(time: string){
+   const date = dayjs().format("YYYY-MM-DD"); //4-14-2023
+
+   const dateTimeFormat = new Date(`${date} ${time}`); //4-14-2023 20:50
+}
