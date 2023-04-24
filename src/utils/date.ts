da269f774600607888cf722e4f24d9d0dfb30f60
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-04-24 08:16:58 -0300

    adding the complete function time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,12 +3,13 @@ import dayjs from "dayjs";
 //! Validate the time and hour from dayjs
 
 
-function validateTime(time: string){
-
+export function validateTime(time: string){
+  return dayjs(formatDateHour(time)).isValid();
 }
 
-function formatDateHour(time: string){
+export function formatDateHour(time: string){
    const date = dayjs().format("YYYY-MM-DD"); //4-14-2023
 
    const dateTimeFormat = new Date(`${date} ${time}`); //4-14-2023 20:50
+   return dayjs(dateTimeFormat)
 }
