47de39b4be832fbf5a572b3dcfa25fc6e6bd81a3
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-05-06 18:55:44 -0300

    fixed up the bug doctor-info and create new controller

--- src/modules/doctor/repositories/implementations/prisma/doctor-prisma.repository.ts
+++ src/modules/doctor/repositories/implementations/prisma/doctor-prisma.repository.ts
@@ -1,33 +1,41 @@
 // import { prisma } from '@prisma/client';
 // import { prismaClient } from '../../../../../infra/databases/prisma.config'
 // import { Doctor } from "../../../../entities/doctor.entity";
-// import { DoctorMapper } from "../../../mapper/create-doctor.map";
-// import { IDoctorRepository } from "../../doctor.repository";
+import { Doctor } from "../../../entities/doctor.entity";
+import { DoctorMapper } from "../../../mapper/create-doctor.map";
+import { IDoctorRepository } from "../../doctor.repository";
+// import { Doctor } from '../../../entities/doctor.entity';
 
-// export class DoctorPrismaRepository implements IDoctorRepository{
-//     async save(data: Doctor): Promise<Doctor> {
-//         throw new Error("Method not implemented.");
+export class DoctorPrismaRepository implements IDoctorRepository{
+    findById(id: string): Promise<Doctor | null> {
+        throw new Error('Method not implemented.');
+    }
+    findByUserId(userId: string): Promise<Doctor | null> {
+        throw new Error('Method not implemented.');
+    }
+    async save(data: Doctor): Promise<Doctor> {
+        throw new Error("Method not implemented.");
 
-//         const doctor = await prismaClient.doctor.create({
-//           data: {
-//             crm: data.crm,
-//             email: data.email,
-//             speciality_id: data.specialityId,
-//             user_id: data.userId
-//           },
-//         })
-//         if(doctor) return prismaToEntityDoctor.createDoctorMapper(doctor);
-//         return null
-//     }
+        // const doctor = await prismaClient.doctor.create({
+        //   data: {
+        //     crm: data.crm,
+        //     email: data.email,
+        //     speciality_id: data.specialityId,
+        //     user_id: data.userId
+        //   },
+        // })
+        // if(doctor) return prismaToEntityDoctor.createDoctorMapper(doctor);
+        // return null
+    }
 
-//     async findByCRM(crm: string): Promise<Doctor | null> {
-//         throw new Error("Method not implemented.");
+    async findByCRM(crm: string): Promise<Doctor | null> {
+        throw new Error("Method not implemented.");
 
-//         const doctor = await primaClient.doctor.findUnique({
-//             where: {
-//                 crm
-//             }
-//         })
-//     }
+        // const doctor = await primaClient.doctor.findUnique({
+        //     where: {
+        //         crm
+        //     }
+        // })
+    }
 
-// }
+}
---
+++ src/modules/doctor/useCases/create-doctor-info/index.ts
@@ -0,0 +1,14 @@
+import { DoctorPrismaRepository } from './../../repositories/implementations/prisma/doctor-prisma.repository';
+
+
+import { DoctorInfoPrismaRepository } from "../../repositories/implementations/prisma/doctor-info.prisma.repository";
+import { CreateDoctorInfoController } from "./create-doctor-info.controller";
+
+const doctorRepository = new DoctorPrismaRepository()
+
+const doctorInfoRepository = new DoctorInfoPrismaRepository();
+
+const createDoctorInfoController = new CreateDoctorInfoController(doctorRepository, doctorInfoRepository);
+
+
+export {createDoctorInfoController}
